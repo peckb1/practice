9b0976f9aa2bfc7d5769adee5fec70712c034374
Brian Peck <catbert321@gmail.com>
2026-03-21 16:36:56 -0700

    advanced graphs

---
+++ src/main/kotlin/leetcode/medium/NetworkDelayTime.kt
@@ -0,0 +1,22 @@
+package leetcode.medium
+
+import leetcode.pathing.Distance
+import leetcode.pathing.Graph
+import leetcode.pathing.dijkstra
+
+class NetworkDelayTime {
+  fun networkDelayTime(times: Array<IntArray>, n: Int, k: Int): Int {
+    val adjacency = Array(n + 1) { mutableListOf<Pair<Int, Distance>>() }
+      .apply {
+        times.forEach { (source, destination, cost) -> this[source].add(Pair(destination, Distance(cost))) }
+      }
+
+    val graph: Graph<Int> = object : Graph<Int> {
+      override fun neighbors(node: Int) = adjacency[node]
+    }
+
+    val nodes = dijkstra(graph, k)
+
+    return if (nodes.size < n) { -1 } else { nodes.maxOfOrNull { it.value }?.value!! }
+  }
+}
---
+++ src/main/kotlin/leetcode/pathing/dijkstra.kt
@@ -0,0 +1,48 @@
+package leetcode.pathing
+
+import java.util.PriorityQueue
+
+@JvmInline
+value class Distance(val value: Int) : Comparable<Distance> {
+    override fun compareTo(other: Distance) = value.compareTo(other.value)
+
+    operator fun plus(other: Distance) = Distance(value + other.value)
+}
+
+interface Graph<Node> {
+    fun neighbors(node: Node): Iterable<Pair<Node, Distance>>
+}
+
+fun <Node> dijkstra(graph: Graph<Node>, start: Node, end: Node? = null): Map<Node, Distance> {
+
+    data class State<Node>(
+        val node: Node,
+        val cost: Distance
+    )
+
+    val dist = mutableMapOf<Node, Distance>().withDefault { Distance(Int.MAX_VALUE) }
+
+    val pq = PriorityQueue<State<Node>>(compareBy { it.cost })
+
+    dist[start] = Distance(0)
+    pq.add(State(start, Distance(0)))
+
+    while (pq.isNotEmpty()) {
+        val (node, cost) = pq.poll()
+
+        if (cost > dist.getValue(node)) continue
+
+        if (end != null && node == end) break
+
+        for ((neighbor, edgeCost) in graph.neighbors(node)) {
+            val newCost = cost + edgeCost
+
+            if (newCost < dist.getValue(neighbor)) {
+                dist[neighbor] = newCost
+                pq.add(State(neighbor, newCost))
+            }
+        }
+    }
+
+    return dist
+}
